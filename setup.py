b3971f023d059b5056bc01585412f224d192269f
Tyler Kelly <107662723+tylerkelly7@users.noreply.github.com>
2025-10-05 16:35:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="TensorFlow",
     version="0.1",
-    packages=find_packages(where="src"),
+    packages=find_packages(where="."),
+    install_requires=[
+    	"tensorflow>=2.10,<3",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "scikit-learn",
+        "pyyaml"
+    ],
+    author="Tyler Kelly",
     package_dir={"": "src"},
 )
